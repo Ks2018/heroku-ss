2b2818dcfaa84aa2b790e9e4b1be4729564f482a
Russell <russell@depop.com>
2017-11-27 16:56:53 +0000

    parse screening date and time into datetime field

--- crawler/scraper/databasepipeline.py
+++ crawler/scraper/databasepipeline.py
@@ -1,12 +1,16 @@
 from web.models import Screening
 
+from dateutil.parser import parse
+
 
 class DatabasePipeline(object):
 
     def process_item(self, item, spider):
+        str = item["date"] + " " + item["time"].replace(".", ":")
+        datetime = parse(str)
         screening = Screening(title=item["title"],
-                              date=item["date"],
-                              time=item["time"],
+                              datetime=datetime,
                               age=item["age"])
+        import ipdb; ipdb.set_trace()
         screening.save()
         return item
--- web/models.py
+++ web/models.py
@@ -2,8 +2,7 @@ from django.db import models
 
 
 class Screening(models.Model):
-    when = models.DateTimeField('date created', auto_now_add=True)
+    created = models.DateTimeField('date created', auto_now_add=True)
     title = models.CharField(max_length=100, unique=True)
-    date = models.CharField(max_length=20)
-    time = models.CharField(max_length=5)
+    datetime = models.DateTimeField()
     age = models.CharField(max_length=5)
